2e1b95667f5d093a50b0b842c5b64649d40d024a
otoko-tech <tedijulian97@gmail.com>
2022-04-17 21:40:05 +0700

    completed

---
+++ frontend/scripts/main.js
@@ -0,0 +1,35 @@
+// $("#video").on("hide.bs.modal", function (event) {
+//     var myPlayer = videojs("my-video");
+//     myPlayer.pause();
+// });
+// $("#videoMobile").on("hide.bs.modal", function (event) {
+//     var myPlayer = videojs("my-video-mobile");
+//     myPlayer.pause();
+// });
+
+// function openNav() {
+//     document.getElementById("myNav").style.width = "100%";
+// }
+
+// /* Close when someone clicks on the "x" symbol inside the overlay */
+// function closeNav() {
+//     document.getElementById("myNav").style.width = "0%";
+// }
+
+const navbar = document.querySelector("#navbar");
+window.onscroll = function () {
+  var top = window.scrollY;
+  if (top > 50) {
+    navbar.classList.add("muncul");
+  } else {
+    navbar.classList.remove("muncul");
+  }
+};
+
+const loc = document.querySelectorAll(".located");
+
+loc.forEach((located) => {
+  located.addEventListener("click", () => {
+    located.classList.toggle("active");
+  });
+});
