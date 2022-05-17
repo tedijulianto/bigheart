ae06707962e0d62887ff1826edd254cbc5f37ecc
tedijulianto <tedijulianto92@gmail.com>
2022-05-17 17:23:01 +0700

    adding file footer.blade

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -667,15 +667,5 @@
   </div>
 </section>
 
-<footer>
-  <div class="row justify-content-center m-0">
-    <div class="col-lg-10">
-      <div class="row justify-content-center">
-        <div class="text-center py-3">
-          <p class="m-0">Copyright © 2022 <span class="red">Bighearts.</span> All rights reserved.</p>
-        </div>
-      </div>
-    </div>
-  </div>
-</footer>
+@include('partials.footer')
 @endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,11 @@
+<footer>
+  <div class="row justify-content-center m-0">
+    <div class="col-lg-10">
+      <div class="row justify-content-center">
+        <div class="text-center py-3">
+          <p class="m-0">Copyright © 2022 <span class="red">Bighearts.</span> All rights reserved.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
